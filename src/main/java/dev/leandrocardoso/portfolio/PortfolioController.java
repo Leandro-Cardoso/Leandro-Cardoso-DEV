d10ec5e6894399ad99890c10582f1cfe135a3d41
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-05-17 20:11:49 -0300

    Index e modulo header e footer

--- src/main/java/dev/leandrocardoso/portfolio/PortfolioController.java
+++ src/main/java/dev/leandrocardoso/portfolio/PortfolioController.java
@@ -11,13 +11,10 @@ public class PortfolioController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("title", "Leandro Carodoso {DEV}");
-        model.addAttribute("h1", "Leandro Carodoso");
+        model.addAttribute("title", "Leandro Cardoso {DEV}");
+        model.addAttribute("h1", "Leandro Cardoso");
         model.addAttribute("header", "modules/header");
         model.addAttribute("divH1", "modules/h1");
-        model.addAttribute("aboutShort", "modules/about-short");
-        model.addAttribute("projectsShort", "modules/projects-short");
-        model.addAttribute("blogShort", "modules/blog-short");
         model.addAttribute("footer", "modules/footer");
         return "index";
     }
